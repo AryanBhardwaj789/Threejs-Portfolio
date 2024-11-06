5101260d0e22d3b522fbb2f3cf52660fa67a0f4a
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-11-06 18:10:56 -0500

    Contact Section Updated

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -11,8 +11,8 @@ const Contact = () => {
         message: ''
     })
 
-    const handleChange = ({target: {name , value}}) => {
-        setForm({...form, [name]: value})
+    const handleChange = ({ target: { name, value } }) => {
+        setForm({ ...form, [name]: value })
     }
 
     // service_8cq00yv
@@ -23,23 +23,24 @@ const Contact = () => {
         setLoading(true);
 
         try {
-            await emailjs.send('service_8cq00yv',
-                "template_e17envg",
+            await emailjs.send(
+                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
                 {
                     from_name: form.name,
                     to_name: 'Aryan',
                     from_email: form.email,
                     to_email: 'aryanbhardwaj98126@gmail.com',
-                    message: form.message
+                    message: form.message,
                 },
-                "WFFuSXUKJ7TcAi4qI"
+                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
             )
             setLoading(false);
             alert('Your message has been sent');
             setForm({
-                name:'',
-                email:'',
-                message:''
+                name: '',
+                email: '',
+                message: ''
             });
         } catch (error) {
             setLoading(false);
@@ -47,62 +48,62 @@ const Contact = () => {
             alert('Something went wrong!');
         }
     }
-    
-  return (
-    <section className='c-space my-20'>
-    <div className='relative min-h-screen flex items-center justify-center flex-col'>
-        <img src="/assets/terminal.png" alt="terminal background" className='absolute inset-0 min-h-screen' />
-        <div className='contact-container'>
-        <h3 className='head-text'>Let's Talk</h3>
-        <p className='text-lg text-white-600 mt-3'>
-            Whether you're looking to build a new website, improve your existing platform, or bring a unique
-            project to life, I'm here to help.
-        </p>
-        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
-            <label className='space-y-3'>
-                <span className='field-label'>Full Name</span>
-                <input 
-                    type="text" 
-                    name='name' 
-                    value={form.name} 
-                    onChange={handleChange} 
-                    required 
-                    className='field-input'
-                    placeholder='John Doe' />
-            </label>
-            <label className='space-y-3'>
-                <span className='field-label'>Email</span>
-                <input 
-                    type="email" 
-                    name='email' 
-                    value={form.email} 
-                    onChange={handleChange} 
-                    required 
-                    className='field-input'
-                    placeholder='johndoe@gmail.com' />
-            </label>
-            <label className='space-y-3'>
-                <span className='field-label'>Your Message</span>
-                <textarea 
-                    name='message' 
-                    value={form.message} 
-                    onChange={handleChange} 
-                    required 
-                    rows={5}
-                    className='field-input'
-                    placeholder="Hi, I wanna give you a job ..." />
-            </label>
-            <button className='field-btn' type='submit' disabled={loading}>
-                {loading ? 'Sending...' : 'Send Message'}
-                <img src="/assets/arrow-up.png" alt="arrow-up" className='field-btn_arrow' />
-            </button>
-        </form>
-        </div>
-    </div>
 
-      
-    </section>
-  )
+    return (
+        <section className='c-space my-20'>
+            <div className='relative min-h-screen flex items-center justify-center flex-col'>
+                <img src="/assets/terminal.png" alt="terminal background" className='absolute inset-0 min-h-screen' />
+                <div className='contact-container'>
+                    <h3 className='head-text'>Let's Talk</h3>
+                    <p className='text-lg text-white-600 mt-3'>
+                        Whether you're looking to build a new website, improve your existing platform, or bring a unique
+                        project to life, I'm here to help.
+                    </p>
+                    <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
+                        <label className='space-y-3'>
+                            <span className='field-label'>Full Name</span>
+                            <input
+                                type="text"
+                                name='name'
+                                value={form.name}
+                                onChange={handleChange}
+                                required
+                                className='field-input'
+                                placeholder='John Doe' />
+                        </label>
+                        <label className='space-y-3'>
+                            <span className='field-label'>Email</span>
+                            <input
+                                type="email"
+                                name='email'
+                                value={form.email}
+                                onChange={handleChange}
+                                required
+                                className='field-input'
+                                placeholder='johndoe@gmail.com' />
+                        </label>
+                        <label className='space-y-3'>
+                            <span className='field-label'>Your Message</span>
+                            <textarea
+                                name='message'
+                                value={form.message}
+                                onChange={handleChange}
+                                required
+                                rows={5}
+                                className='field-input'
+                                placeholder="Hi, I wanna give you a job ..." />
+                        </label>
+                        <button className='field-btn' type='submit' disabled={loading}>
+                            {loading ? 'Sending...' : 'Send Message'}
+                            <img src="/assets/arrow-up.png" alt="arrow-up" className='field-btn_arrow' />
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+
+        </section>
+    )
 }
 
 export default Contact
